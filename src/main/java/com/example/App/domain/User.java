a98527a01b394144f1072d2a92236272bcf380aa
viktartromza <viktar.tromza@gmail.com>
2023-03-01 21:07:41 +0300

    Initial commit

---
+++ src/main/java/com/example/App/domain/User.java
@@ -0,0 +1,21 @@
+package com.example.App.domain;
+
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+public class User {
+    private int id;
+    private String login;
+    private String password;
+    private Date regDate;
+    private String email;
+    private int score;
+
+    private String firstName;
+    private String lastName;
+    private Date birthDay;
+    private String telephone;
+    private double balance;
+}
